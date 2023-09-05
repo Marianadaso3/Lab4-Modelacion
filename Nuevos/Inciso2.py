d71288a491dad82c3bb2cab3af8167f33d6ac624
Marianadaso3 <72479030+Marianadaso3@users.noreply.github.com>
2023-09-05 01:19:43 -0600

    Inciso 2 DONE :)

--- Nuevos/Inciso2.py
+++ Nuevos/Inciso2.py
@@ -1,86 +1,68 @@
+#Importaciones
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Número de estudiantes
-total_students = 1000000
+#Inciso A------------------------------------------------
 
-# Probabilidades
-probability_woman = 400000 / total_students
-probability_glasses = 300000 / total_students
-probability_man_with_glasses = 150000 / total_students
+# Función para generar valores de la distribución de Poisson con parámetro lambda
+def generate_poisson(lambda_value):
+    k = 0
+    p = 1.0
+    while True:
+        k += 1
+        p *= random.random()
+        if p <= np.exp(-lambda_value):
+            break
+    return k
 
-# Función para generar una persona al azar con base en la transformada inversa
-def generate_random_person():
-    r1 = random.uniform(0, 1)  # Generamos un número aleatorio entre 0 y 1 para el género
-    r2 = random.uniform(0, 1)  # Generamos otro número aleatorio entre 0 y 1 para los lentes
-    
-    if r1 <= probability_woman:
-        gender = 'Mujer'
-    else:
-        gender = 'Varón'
-    
-    if r2 <= probability_glasses:
-        glasses = 'Usa lentes'
-    else:
-        glasses = 'No usa lentes'
-    
-    return gender, glasses
+# Número de valores a generar
+n = 100000
 
-# Simulación de "Probabilidad de ser mujer y usar gafas"
-count_woman_and_glasses = 0
-num_simulations = 10000  # Reducido para acelerar la simulación y el gráfico
+# Parámetro de la distribución de Poisson
+lambda_value = 5
 
-# Lista para almacenar las probabilidades simuladas a lo largo de las simulaciones
-simulated_probabilities_woman_and_glasses = []
+# Generar valores aleatorios y calcular las medias parciales
+values = [generate_poisson(lambda_value) for _ in range(n)]
+partial_means = [sum(values[:i+1]) / (i+1) for i in range(n)]
 
-for _ in range(num_simulations):
-    gender, glasses = generate_random_person()
-    if gender == 'Mujer' and glasses == 'Usa lentes':
-        count_woman_and_glasses += 1
-    
-    # Calculamos la probabilidad simulada hasta el momento y la agregamos a la lista
-    probability_so_far = count_woman_and_glasses / (_ + 1)
-    simulated_probabilities_woman_and_glasses.append(probability_so_far)
+# Calcular la media real de la distribución de Poisson
+real_mean = lambda_value
 
-# Simulación de "Probabilidad de ser varón dado que no usa lentes"
-count_man_no_glasses = 0
-
-# Lista para almacenar las probabilidades simuladas a lo largo de las simulaciones
-simulated_probabilities_man_no_glasses = []
-
-for _ in range(num_simulations):
-    gender, glasses = generate_random_person()
-    if gender == 'Varón' and glasses == 'No usa lentes':
-        count_man_no_glasses += 1
-    
-    # Calculamos la probabilidad simulada hasta el momento y la agregamos a la lista
-    probability_so_far = count_man_no_glasses / (_ + 1)
-    simulated_probabilities_man_no_glasses.append(probability_so_far)
-
-# Mostrar los resultados de las probabilidades calculadas en la consola
-print(f"Probabilidad simulada de ser mujer y usar gafas: {simulated_probabilities_woman_and_glasses[-1]}")
-print(f"Probabilidad simulada de ser varón dado que no usa lentes: {simulated_probabilities_man_no_glasses[-1]}")
-
-# Crear gráficos
-plt.figure(figsize=(12, 6))
-
-# Gráfico para "Probabilidad de ser mujer y usar gafas"
-plt.subplot(1, 2, 1)
-plt.plot(range(1, num_simulations + 1), simulated_probabilities_woman_and_glasses, label='Simulado')
-plt.axhline(y=probability_man_with_glasses, color='r', linestyle='--', label='Teórico')
-plt.xlabel('Número de simulaciones')
-plt.ylabel('Probabilidad')
-plt.title('Probabilidad simulada de ser mujer y usar gafas')
+# Graficar las medias parciales y la media real
+plt.plot(range(1, n+1), partial_means, label='Medias Parciales')
+plt.axhline(y=real_mean, color='r', linestyle='--', label='Media Real')
+plt.xlabel('n (Tamaño de la muestra)')
+plt.ylabel('Media')
+plt.title('Convergencia de Medias Parciales a la Media Real (Distribución de Poisson)')
 plt.legend()
+plt.show()
 
-# Gráfico para "Probabilidad de ser varón dado que no usa lentes"
-plt.subplot(1, 2, 2)
-plt.plot(range(1, num_simulations + 1), simulated_probabilities_man_no_glasses, label='Simulado')
-plt.axhline(y=0.6, color='r', linestyle='--', label='Teórico')  # Corrección aquí
-plt.xlabel('Número de simulaciones')
-plt.ylabel('Probabilidad')
-plt.title('Probabilidad simulada de ser varón dado que no usa lentes')
+
+#Inciso B------------------------------------------------
+
+# Función para generar valores de la distribución exponencial con parámetro lambda
+def generate_exponential(lambda_value):
+    return -np.log(1 - random.random()) / lambda_value
+
+# Número de valores a generar
+n = 10000
+
+# Parámetro de la distribución exponencial
+lambda_value = 2
+
+# Generar valores aleatorios y calcular las medias parciales
+values = [generate_exponential(lambda_value) for _ in range(n)]
+partial_means = [sum(values[:i+1]) / (i+1) for i in range(n)]
+
+# Calcular la media real de la distribución exponencial
+real_mean = 1 / lambda_value
+
+# Graficar las medias parciales y la media real
+plt.plot(range(1, n+1), partial_means, label='Medias Parciales')
+plt.axhline(y=real_mean, color='r', linestyle='--', label='Media Real')
+plt.xlabel('n (Tamaño de la muestra)')
+plt.ylabel('Media')
+plt.title('Convergencia de Medias Parciales a la Media Real (Distribución Exponencial)')
 plt.legend()
-
-plt.tight_layout()
 plt.show()
